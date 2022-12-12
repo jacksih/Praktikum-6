e996596e86896a3527e71886a49dd9c17f78fa7d
Jacky Sihombing <91112863+jacksih@users.noreply.github.com>
2022-12-12 13:51:42 +0700

    Create praktikum6.php

---
+++ praktikum6.php
@@ -0,0 +1,36 @@
+<?php 
+function to_roman($number) {
+    $map = array(
+        'M' => 1000,
+        'CM' => 900,
+        'D' => 500,
+        'CD' => 400,
+        'C' => 100,
+        'XC' => 90,
+        'L' => 50,
+        'XL' => 40,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1
+    );
+
+    $returnValue = '';
+    while ($number > 0) {
+        foreach ($map as $roman => $int) {
+            if($number >= $int) {
+                $number -= $int;
+                $returnValue .= $roman;
+                break;
+            }
+        }
+    }
+
+    return $returnValue;
+}
+$number = 500;
+$roman = to_roman($number);
+echo $roman; // output: IV
+?>
+
